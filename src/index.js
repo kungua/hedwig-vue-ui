db933ffe22e15f1df38cd789573d27c5ee78024d
kungua <maguahongkun@outlook.com>
2019-10-30 19:23:42 +0800

    fix: typo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export {default as Icon} from './Icon'
-export {default as Image} from 'Image'
+export {default as Image} from './Image'
 export {default as ImagesViewer} from './ImagesViewer'
 export {default as Loading} from './Loading'
